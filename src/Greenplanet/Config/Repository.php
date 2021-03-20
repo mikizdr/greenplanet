7fa0476db5a7d3d1758101f3d3f7e1db6fa85658
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2021-03-21 00:56:03 +0100

    Initial commit

---
+++ src/Greenplanet/Config/Repository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Greenplanet\Config;
+
+/**
+ * Repository class.
+ * 
+ * @author  Miroslav Zdravkovic  <miroslav.zdravkovic@gmail.com>
+ * 
+ * @method string green()
+ */
+class Repository
+{
+    /**
+     * Returns hello for testing
+     *
+     * @return string
+     */
+    public static function green(): string
+    {
+        return 'Hello Green Planet!';
+    }
+
+}
